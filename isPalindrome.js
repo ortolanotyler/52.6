563dc01dc52b8f13f255f74d3bfe1c62fb0c92e6
Tyler Ortolano <111775236+ortolanotyler@users.noreply.github.com>
2023-11-17 10:41:39 -0500

    Create isPalindrome.js

---
+++ isPalindrome.js
@@ -0,0 +1,5 @@
+function isPalindrome(str, start = 0, end = str.length - 1) {
+  if (start >= end) return true;
+  if (str[start] !== str[end]) return false;
+  return isPalindrome(str, start + 1, end - 1);
+}
